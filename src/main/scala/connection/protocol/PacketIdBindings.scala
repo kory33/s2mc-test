d984c555f614bc4522c7df6e2d31bdfe54dbbf1a
Kory <korygm33@gmail.com>
2021-08-28 08:26:21 +0900

    clean: reimplement PacketIdBindings without using macro

--- src/main/scala/connection/protocol/PacketIdBindings.scala
+++ src/main/scala/connection/protocol/PacketIdBindings.scala
@@ -2,26 +2,57 @@ package com.github.kory33.s2mctest
 package connection.protocol
 
 import connection.protocol.codec.{ByteCodec, ByteDecode}
-import generic.TuplePosition
-
-import scala.Tuple.Map
 
 type PacketId = Int
 type CodecBinding[A] = (PacketId, ByteCodec[A])
 
-class PacketIdBindings[Tup <: NonEmptyTuple](bindings: Tuple.Map[Tup, CodecBinding]) {
-  def decoderFor(id: PacketId): Option[ByteDecode[Tuple.Union[Tup]]] =
+class PacketIdBindings[BindingTup <: NonEmptyTuple](bindings: BindingTup)
+                                                   (using ev: Tuple.IsMappedBy[CodecBinding][BindingTup]) {
+  def decoderFor(id: PacketId): Option[ByteDecode[Tuple.Union[Tuple.InverseMap[BindingTup, CodecBinding]]]] =
+    // we can't use these types as type parameter bound or return type
+    // because compiler does not reduce them to concrete types for some reason
+    type PacketTuple = Tuple.InverseMap[BindingTup, CodecBinding]
+    type Packet = Tuple.Union[PacketTuple]
+
     bindings
       .toList
       // each element `e` of `mapping.toList` satisfies
       // `e: (PacketID, ByteCodec[_ <: Tuple.Union[Tup]]`
       // but `(PacketID, ByteCodec[_ <: Tuple.Union[Tup]] <: (PacketID, ByteDecode[Tuple.Union[Tup]])`
       // so this cast is safe
-      .asInstanceOf[List[(PacketId, ByteDecode[Tuple.Union[Tup]])]]
+      .asInstanceOf[List[(PacketId, ByteDecode[Tuple.Union[Tuple.InverseMap[BindingTup, CodecBinding]]])]]
       .find { case (i, _) => i == id }
       .map { case (_, decoder) => decoder }
 
-  inline def encodeKnown[O <: Tuple.Union[Tup]](obj: O): (PacketId, fs2.Chunk[Byte]) =
-    val (id, codec) = TuplePosition[Tup, O].extract[CodecBinding](bindings)
+  /**
+   * Statically resolve the codec associated with type [[O]].
+   *
+   * Altough [[O]] has union of packet types as upper bound, it is required that
+   * a type equivalent to `CodecBinding[O]` appears in [[BindingTup]].
+   * For example, if [[BindingTup]] is [[((Int, CodecBinding[Int]), CodecBinding[String])]],
+   * `encodeKnown` accepts `Int | String` as the type parameter but it produces a compilation error.
+   */
+  inline def getFirstBindingOf[O <: Tuple.Union[Tuple.InverseMap[BindingTup, CodecBinding]]]: CodecBinding[O] =
+    import scala.compiletime.constValue
+    import scala.compiletime.ops.int.S
+
+    type IndexOfCodecBindingIn[O, Tup] <: Int = Tup match
+      case CodecBinding[O] *: tail => 0
+      case _ *: tail => S[IndexOfCodecBindingIn[O, tail]]
+
+    // if IndexOfCodecBindingIn[O, BindingTup] is const with value `index`,
+    // `bindings` must contain CodecBinding[O] at `index`
+    bindings(constValue[IndexOfCodecBindingIn[O, BindingTup]]).asInstanceOf[CodecBinding[O]]
+
+  /**
+   * Statically encode a packet object using [[bindings]] provided.
+   *
+   * Altough [[O]] has union of packet types as upper bound, it is required that
+   * a type equivalent to `CodecBinding[O]` appears in [[BindingTup]].
+   * For example, if [[BindingTup]] is [[((Int, CodecBinding[Int]), CodecBinding[String])]],
+   * `encodeKnown` accepts `Int | String` as the type parameter but it produces a compilation error.
+   */
+  inline def encodeKnown[O <: Tuple.Union[Tuple.InverseMap[BindingTup, CodecBinding]]](obj: O): (PacketId, fs2.Chunk[Byte]) =
+    val (id, codec) = getFirstBindingOf[O]
     (id, codec.write(obj))
 }
