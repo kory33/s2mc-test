735916907a47c87f372249cd6b75831a1441e708
Kory <korygm33@gmail.com>
2021-10-08 17:09:58 +0100

    delete: remove App from core module

--- core/src/main/scala/com/github/kory33/s2mctest/core/App.scala
+++
@@ -1,9 +0,0 @@
-package com.github.kory33.s2mctest.core
-
-import cats.effect.{ExitCode, IO, IOApp}
-
-object App extends IOApp {
-
-  def run(args: List[String]): IO[ExitCode] = IO.pure(ExitCode.Success)
-
-}
