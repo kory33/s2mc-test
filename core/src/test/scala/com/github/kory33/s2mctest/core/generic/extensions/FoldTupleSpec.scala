95d393762e267b43c06180f2fe0febee2d2e9733
Kory <korygm33@gmail.com>
2021-10-09 06:02:11 +0100

    feat: add tests for FoldTuple

---
+++ core/src/test/scala/com/github/kory33/s2mctest/core/generic/extensions/FoldTupleSpec.scala
@@ -0,0 +1,27 @@
+package com.github.kory33.s2mctest.core.generic.extensions
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class FoldTupleSpec extends AnyFlatSpec with should.Matchers {
+  type Pair = [a] =>> (a, a)
+
+  import FoldTuple.*
+
+  "foldLeft" should "fold a tuple to a value" in {
+    def foldToString[F[_], BaseTuple <: Tuple](tuple: Tuple.Map[BaseTuple, F]): String =
+      foldLeft[F, BaseTuple](tuple)("")([t] => (str: String, next: F[t]) => s"$str${next.toString} *: ") + "TNil"
+
+    assert(
+      foldToString[Pair, (Int, 42, String)](((1, 3), (42, 42), ("ab", "cd"))) ==
+        "(1,3) *: (42,42) *: (ab,cd) *: TNil"
+    )
+  }
+
+  "foldToList" should "accumulate a tuple into a list" in {
+    assert {
+      foldToList[Pair, (Int, 42, String)]((1, 3), (42, 42), ("ab", "cd"))([t] => (next: Pair[t]) => next: Any) ==
+        List((1, 3), (42, 42), ("ab", "cd"))
+    }
+  }
+}
