f67d3600d6139074f6c423a25c73caf79364f62f
Kory <korygm33@gmail.com>
2021-10-09 21:03:02 +0100

    feat(core): add ProtocolBasedTransport's definition

---
+++ core/src/main/scala/com/github/kory33/s2mctest/core/connection/transport/ProtocolBasedTransport.scala
@@ -0,0 +1,31 @@
+package com.github.kory33.s2mctest.core.connection.transport
+
+import cats.Functor
+import com.github.kory33.s2mctest.core.connection.protocol.ProtocolView
+import com.github.kory33.s2mctest.core.connection.protocol.CodecBinding
+
+// format: off
+case class ProtocolBasedTransport[
+  F[_]: Functor,
+  SelfBoundBindings <: Tuple, PeerBoundBindings <: Tuple
+](
+  transport: PacketTransport[F],
+  protocolView: ProtocolView[SelfBoundBindings, PeerBoundBindings]
+) {
+// format: on
+
+  type PacketIn[T <: Tuple] = Tuple.Union[Tuple.InverseMap[T, CodecBinding]]
+
+  def nextPacket: F[PacketIn[SelfBoundBindings]] = ???
+
+  import com.github.kory33.s2mctest.core.generic.compiletime.*
+
+  def writePacketWithBindingsIndex[P, Idx <: Int](peerBoundPacket: P, idx: Idx)(
+    using Tuple.Elem[PeerBoundBindings, Idx] =:= CodecBinding[P]
+  ): F[Unit] = ???
+
+  inline def writePacket[P](peerBoundPacket: P)(
+    using IncludedInT[PeerBoundBindings, P]
+  ): F[Unit] = ???
+
+}
