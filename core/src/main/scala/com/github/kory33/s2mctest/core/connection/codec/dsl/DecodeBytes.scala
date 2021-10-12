fce2756a1d104db0198b580ce10e4e9f5810a2f6
Kory <korygm33@gmail.com>
2021-10-12 13:03:34 +0100

    feat(core): add Decode(Finite)Bytes DSL definitions

---
+++ core/src/main/scala/com/github/kory33/s2mctest/core/connection/codec/dsl/DecodeBytes.scala
@@ -0,0 +1,60 @@
+package com.github.kory33.s2mctest.core.connection.codec.dsl
+
+import cats.free.Free
+
+/**
+ * The instruction set for an embedded domain specific language (eDSL) that is able to express
+ * the process of
+ *   - reading chunks of binary data from some (possibly infinite) binary data source
+ *   - raising parse errors
+ *   - purely transforming obtained chunks into useful data structures
+ */
+enum ReadBytesInstruction[R]:
+  /**
+   * An instruction to read precisely [[n]] bytes from the data source. If an interpreter cannot
+   * fulfill this requirement, it must interrupt the execution (i.e. throw with EitherT
+   * capability or early-return with OptionT capability).
+   */
+  case ReadWithSize(n: Int) extends ReadBytesInstruction[fs2.Chunk[Byte] /* Size: n */ ]
+
+  /**
+   * An instruction to abort parsing of the data source for an encounter with invalid input
+   * data.
+   *
+   * This instruction does not read any input.
+   */
+  case RaiseError(error: Throwable) extends ReadBytesInstruction[Nothing]
+
+  /**
+   * An instruction to give up parsing the data source for not knowing how to parse additional
+   * data.
+   *
+   * This instruction does not read any input.
+   *
+   * This is semantically different from [[SignalInvalidData]], because [[SignalInvalidData]]
+   * says that input data is known to be invalid while [[GiveUp]] says that the data is in an
+   * unknown format.
+   */
+  case GiveUp(reason: String) extends ReadBytesInstruction[Nothing]
+
+/**
+ * Programs of ReadBytes DSL.
+ *
+ * ReadBytes DSL itself has a free monadic structure, but a sensible interpreter (other than
+ * that injects programs into another free monad) should satisfy the following law:
+ * {{{
+ *   read(0) <==> pure(fs2.Chunk.empty[Byte]); // (read(0)-pure)
+ * }}}
+ * where `<==>` denotes a semantic equivalence.
+ */
+type DecodeBytes[A] = Free[ReadBytesInstruction, A]
+
+object DecodeBytes:
+  def read(n: Int): DecodeBytes[fs2.Chunk[Byte]] =
+    Free.liftF(ReadBytesInstruction.ReadWithSize(n))
+
+  def raiseError(error: Throwable): DecodeBytes[Nothing] =
+    Free.liftF(ReadBytesInstruction.RaiseError(error))
+
+  def giveUp(reason: String): DecodeBytes[Nothing] =
+    Free.liftF(ReadBytesInstruction.GiveUp(reason))
---
+++ core/src/main/scala/com/github/kory33/s2mctest/core/connection/codec/dsl/DecodeFiniteBytes.scala
@@ -0,0 +1,51 @@
+package com.github.kory33.s2mctest.core.connection.codec.dsl
+
+import cats.data.EitherK
+import cats.free.Free
+
+/**
+ * The instructions present in [[DecodeFiniteBytes]], except those already present in
+ * [[ReadBytesInstruction]].
+ */
+enum ReadFiniteDataInstruction[T]:
+  /**
+   * An instruction to read all bytes until the end of the data source.
+   */
+  case ReadUntilTheEnd extends ReadFiniteDataInstruction[fs2.Chunk[Byte]]
+
+/**
+ * The instruction set for an embedded domain specific language (eDSL) to read bytes from a
+ * *finite* sequence of bytes.
+ */
+type DecodeFiniteBytesInstructions[A] =
+  EitherK[ReadBytesInstruction, ReadFiniteDataInstruction, A]
+
+/**
+ * Programs of ReadDelimitedBytes DSL.
+ *
+ * ReadDelimitedBytes DSL itself has a free monadic structure, but a sensible interpreter (other
+ * than that injects programs into another free monad) should satisfy the following law:
+ * {{{
+ *   read(0) <==> pure(fs2.Chunk.empty[Byte]); // (read(0)-idempotent)
+ *
+ *   readUntilTheEnd << readUntilTheEnd <==> readUntilTheEnd // (readUntilTheEnd-<<-idempotent)
+ *   readUntilTheEnd >> read(n) <==> readUntilTheEnd >> read(0) // (readUntilTheEnd-read)
+ * }}}
+ * where `<==>` denotes a semantic equivalence.
+ */
+type DecodeFiniteBytes[A] = Free[DecodeFiniteBytesInstructions, A]
+
+object DecodeFiniteBytes:
+  import cats.implicits.given
+
+  def read(n: Int): DecodeFiniteBytes[fs2.Chunk[Byte]] =
+    DecodeBytes.read(n).inject
+
+  val readUntilNextMark: DecodeFiniteBytes[fs2.Chunk[Byte]] =
+    Free.liftInject(ReadFiniteDataInstruction.ReadUntilTheEnd)
+
+  def raiseError(error: Throwable): DecodeBytes[Nothing] =
+    Free.liftF(ReadBytesInstruction.RaiseError(error))
+
+  def giveUp(reason: String): DecodeBytes[Nothing] =
+    Free.liftF(ReadBytesInstruction.GiveUp(reason))
