8174d0b27530592ec5f9c1b0b0d6662d1e2309cf
Kory <korygm33@gmail.com>
2021-10-13 11:48:24 +0100

    refactor(impl): move impl classes into s2mctest.impl.connection

---
+++ impl/src/main/scala/com/github/kory33/s2mctest/impl/connection/CommonProtocol.scala
@@ -0,0 +1,43 @@
+package com.github.kory33.s2mctest.impl.connection
+
+import com.github.kory33.s2mctest.core.connection.codec.ByteCodec
+import com.github.kory33.s2mctest.core.connection.protocol.{PacketIdBindings, Protocol}
+import com.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Handshaking.ServerBound.Handshake
+import com.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Status.ServerBound.{
+  StatusPing,
+  StatusRequest
+}
+
+object CommonProtocol {
+
+  import com.github.kory33.s2mctest.impl.connection.codec.ByteCodecs.Common.given
+  import com.github.kory33.s2mctest.impl.connection.macros.GenByteDecode.given
+
+  object common {
+    import com.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Handshaking.ServerBound.*
+    import com.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Status.ClientBound.*
+    import com.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Status.ServerBound.*
+
+    // format: off
+    val handshakeProtocol = Protocol(
+      PacketIdBindings(Tuple(
+        0x00 -> ByteCodec.summonPair[Handshake],
+      )),
+      PacketIdBindings(Tuple())
+    )
+    // format: on
+
+    // format: off
+    val statusProtocol = Protocol(
+      PacketIdBindings((
+        0x00 -> ByteCodec.summonPair[StatusRequest],
+        0x01 -> ByteCodec.summonPair[StatusPing],
+      )),
+      PacketIdBindings((
+        0x00 -> ByteCodec.summonPair[StatusResponse],
+        0x01 -> ByteCodec.summonPair[StatusPong],
+      ))
+    )
+    // format: on
+  }
+}
--- impl/src/main/scala/com/github/kory33/s2mctest/protocol/impl/typeclass/RaiseThrowable.scala
+++
@@ -1,6 +0,0 @@
-package com.github.kory33.s2mctest.protocol.impl.typeclass
-
-type RaiseThrowable[F[_]] = cats.mtl.Raise[F, Throwable]
-object RaiseThrowable {
-  def apply[F[_]](using ev: RaiseThrowable[F]): RaiseThrowable[F] = ev
-}
