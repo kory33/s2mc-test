cdac81bc1fb7e7da9d345371f8d19e8347b3732d
Kory <korygm33@gmail.com>
2021-10-14 02:45:25 +0100

    fix(core): fix compilation errors that appear when invoking writePacket

--- core/src/main/scala/com/github/kory33/s2mctest/core/connection/transport/ProtocolBasedTransport.scala
+++ core/src/main/scala/com/github/kory33/s2mctest/core/connection/transport/ProtocolBasedTransport.scala
@@ -59,11 +59,29 @@ case class ProtocolBasedTransport[
    * are both concrete at the call site.
    */
   inline def writePacket[P](peerBoundPacket: P)(
-    // this constraint will ensure that idx is materialized at compile time
-    using IncludedInT[PeerBoundBindings, P]
+    // this constraint will ensure that idx can be materialized at compile time
+    using Require[IncludedInT[PeerBoundBindings, CodecBinding[P]]]
   ): F[Unit] =
-    type Idx = IndexOfT[P, PeerBoundBindings]
-    val idx: Idx = scala.compiletime.constValue[Idx]
-    writePacketWithBindingsIndex(peerBoundPacket, idx)(using scala.compiletime.summonInline)
+    val idx = scala.compiletime.constValue[IndexOfT[CodecBinding[P], PeerBoundBindings]]
+
+    // PeerBoundBindings & NonEmptyTuple is guaranteed to be a concrete tuple type,
+    // because CodecBinding[P] is included in PeerBoundBindings so it must be nonempty.
+    //
+    // By Require[IncludedInT[...]] constraint, IndexOfT[CodecBinding[P], PeerBoundBindings]
+    // reduces to a singleton type of integer at which PeerBoundBindings has CodecBinding[P],
+    // so this summoning succeeds.
+    val ev: Tuple.Elem[PeerBoundBindings & NonEmptyTuple, IndexOfT[CodecBinding[
+      P
+    ], PeerBoundBindings]] =:= CodecBinding[P] =
+      scala.compiletime.summonInline
+
+    // We know that IndexOfT[CodecBinding[P], PeerBoundBindings] and idx.type will reduce to
+    // the same integer types, but somehow Scala 3.0.1 compiler does not seem to recognize this.
+    // Hence the asInstanceOf cast.
+    // TODO can we get rid of this?
+    val ev1: Tuple.Elem[PeerBoundBindings & NonEmptyTuple, idx.type] =:= CodecBinding[P] =
+      ev.asInstanceOf
+
+    writePacketWithBindingsIndex(peerBoundPacket, idx)(using ev1)
 
 }
