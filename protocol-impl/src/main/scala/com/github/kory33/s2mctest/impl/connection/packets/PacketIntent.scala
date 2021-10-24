3882849c4a78bc92d7988e493cc2d86d3eb10aa8
Kory <korygm33@gmail.com>
2021-10-24 14:34:47 +0100

    chore: add full license

--- protocol-impl/src/main/scala/com/github/kory33/s2mctest/impl/connection/packets/PacketIntent.scala
+++ protocol-impl/src/main/scala/com/github/kory33/s2mctest/impl/connection/packets/PacketIntent.scala
@@ -12,12 +12,13 @@ import PacketDataCompoundTypes.*
 
 /**
  * -- from Stevenarella(https://github.com/iceiix/stevenarella),
- * -- which is based on Steven (https://github.com/thinkofname/steven) Permission is hereby
- * granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
+ * -- which is based on Steven (https://github.com/thinkofname/steven)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
