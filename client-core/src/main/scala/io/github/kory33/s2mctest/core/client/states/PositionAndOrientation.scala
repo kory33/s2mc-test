07f1ed09a2bc7e0222e4aac2b421651960220cf2
Kory <korygm33@gmail.com>
2021-10-26 09:25:58 +0100

    feat: add abstraction for player positions

---
+++ client-core/src/main/scala/io/github/kory33/s2mctest/core/client/states/PositionAndOrientation.scala
@@ -0,0 +1,6 @@
+package io.github.kory33.s2mctest.core.client.states
+
+/**
+ * The datatype describing the position and orientation of an entity.
+ */
+case class PositionAndOrientation(x: Double, y: Double, z: Double, yaw: Float, pitch: Float)
---
+++ client-impl/src/main/scala/io/github/kory33/s2mctest/impl/client/abstraction/PlayerPositionAbstraction.scala
@@ -0,0 +1,41 @@
+package io.github.kory33.s2mctest.impl.client.abstraction
+
+import io.github.kory33.s2mctest.core.client.PacketAbstraction
+import io.github.kory33.s2mctest.core.client.states.PositionAndOrientation
+import io.github.kory33.s2mctest.core.connection.transport.ProtocolBasedTransport
+import io.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Play.ClientBound.TeleportPlayer_WithConfirm
+import io.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Play.ServerBound.TeleportConfirm
+
+object PlayerPositionAbstraction {
+
+  /**
+   * An abstraction of player teleport packets that automatically updates
+   * [[PositionAndOrientation]] of the client.
+   */
+  def withConfirmPacket(transport: ProtocolBasedTransport[?, ?, ?])(
+    using transport.protocolView.peerBound.CanEncode[TeleportConfirm]
+  ): PacketAbstraction[TeleportPlayer_WithConfirm, PositionAndOrientation, List[
+    transport.Response
+  ]] = {
+    case TeleportPlayer_WithConfirm(x, y, z, yaw, pitch, flags, teleportId) =>
+      Some { original =>
+        val rawFlags = flags.asRawByte
+        val newPositionAndOrientation = {
+          // see https://wiki.vg/index.php?title=Protocol&oldid=16681#Player_Position_And_Look_.28clientbound.29
+          // for details
+          PositionAndOrientation(
+            // format: off
+            if (rawFlags & 0x01)  == 0 then x     else original.x     + x,
+            if (rawFlags & 0x02)  == 0 then y     else original.y     + y,
+            if (rawFlags & 0x04)  == 0 then z     else original.z     + z,
+            if (rawFlags & 0x08)  == 0 then yaw   else original.yaw   + yaw,
+            if (rawFlags & 0x010) == 0 then pitch else original.pitch + pitch
+            // format: on
+          )
+        }
+
+        (newPositionAndOrientation, List(transport.Response(TeleportConfirm(teleportId))))
+      }
+  }
+
+}
