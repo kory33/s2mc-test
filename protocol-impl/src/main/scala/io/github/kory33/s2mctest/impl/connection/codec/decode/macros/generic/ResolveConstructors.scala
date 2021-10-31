e4ae990c3dc9f960a3118baa28ca2a708bbaea0a
Kory <korygm33@gmail.com>
2021-10-31 20:59:55 +0000

    refactor: remove primaryConstructorHasTypeParameter

--- protocol-impl/src/main/scala/io/github/kory33/s2mctest/impl/connection/codec/decode/macros/generic/ResolveConstructors.scala
+++ protocol-impl/src/main/scala/io/github/kory33/s2mctest/impl/connection/codec/decode/macros/generic/ResolveConstructors.scala
@@ -3,19 +3,6 @@ package io.github.kory33.s2mctest.impl.connection.codec.decode.macros.generic
 import scala.quoted.*
 
 object ResolveConstructors {
-  def primaryConstructorHasTypeParameter[A: Type](using quotes: Quotes): Boolean = {
-    import quotes.reflect.*
-    val DefDef(_, paramClauses, _, _) = TypeRepr.of[A].typeSymbol.primaryConstructor.tree
-    paramClauses match
-      case firstClause :: _ =>
-        // needs an upcast because 3.0.0 compiler fails to infer that head: TypeDef | ValDef
-        (firstClause.params: List[ValDef | TypeDef]) match
-          case TypeDef(_, _) :: _ => true // found type parameter declaration
-          case _ =>
-            false // a type parameter clause cannot be empty, so must be free of type parameters
-      case _ => false // no clause found, so must be free of type parameters
-  }
-
   def primaryConstructorTermOf[A](
     using quotes: Quotes,
     AType: Type[A]
