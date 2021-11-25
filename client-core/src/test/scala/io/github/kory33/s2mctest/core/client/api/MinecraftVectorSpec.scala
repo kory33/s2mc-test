7ce9df981160b7829120cbdc7822f3d92a205c8f
Kory <korygm33@gmail.com>
2021-11-25 07:41:58 +0000

    fix: use Float for spread specification in MinecraftVectorSpec

--- client-core/src/test/scala/io/github/kory33/s2mctest/core/client/api/MinecraftVectorSpec.scala
+++ client-core/src/test/scala/io/github/kory33/s2mctest/core/client/api/MinecraftVectorSpec.scala
@@ -5,33 +5,33 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class MinecraftVectorSpec extends AnyFlatSpec with should.Matchers {
-  val error = 0.00000000001
+  val error = 0.000000001f
 
   "yaw" should "compute yaw values for vectors along axes" in {
-    MinecraftVector(0.0, 3.0, 1.0).yaw should ===(0.0 +- error)
-    MinecraftVector(-1.0, 3.0, 0.0).yaw should ===(90.0 +- error)
-    MinecraftVector(0.0, 3.0, -1.0).yaw should ===(180.0 +- error)
-    MinecraftVector(1.0, 3.0, 0.0).yaw should ===(270.0 +- error)
+    MinecraftVector(0.0, 3.0, 1.0).yaw should ===(0.0f +- error)
+    MinecraftVector(-1.0, 3.0, 0.0).yaw should ===(90.0f +- error)
+    MinecraftVector(0.0, 3.0, -1.0).yaw should ===(180.0f +- error)
+    MinecraftVector(1.0, 3.0, 0.0).yaw should ===(270.0f +- error)
   }
 
   it should "compute yaw values for vectors with intermediate angles" in {
-    MinecraftVector(-1.0, 0.0, 1.0).yaw should ===(45.0 +- error)
-    MinecraftVector(-1.0, 0.0, -1.0).yaw should ===(135.0 +- error)
-    MinecraftVector(1.0, 0.0, -1.0).yaw should ===(225.0 +- error)
-    MinecraftVector(1.0, 0.0, 1.0).yaw should ===(315.0 +- error)
+    MinecraftVector(-1.0, 0.0, 1.0).yaw should ===(45.0f +- error)
+    MinecraftVector(-1.0, 0.0, -1.0).yaw should ===(135.0f +- error)
+    MinecraftVector(1.0, 0.0, -1.0).yaw should ===(225.0f +- error)
+    MinecraftVector(1.0, 0.0, 1.0).yaw should ===(315.0f +- error)
   }
 
   "pitch" should "compute pitch values for vectors along axes" in {
-    MinecraftVector(0.0, 3.0, 0.0).pitch should ===(-90.0 +- error)
-    MinecraftVector(1.0, 0.0, 1.0).pitch should ===(0.0 +- error)
-    MinecraftVector(0.0, -3.0, 0.0).pitch should ===(90.0 +- error)
+    MinecraftVector(0.0, 3.0, 0.0).pitch should ===(-90.0f +- error)
+    MinecraftVector(1.0, 0.0, 1.0).pitch should ===(0.0f +- error)
+    MinecraftVector(0.0, -3.0, 0.0).pitch should ===(90.0f +- error)
   }
 
   it should "compute pitch values for vectors with intermediate angles" in {
-    MinecraftVector(1.0, 1.0, 0.0).pitch should ===(-45.0 +- error)
-    MinecraftVector(0.0, 1.0, 1.0).pitch should ===(-45.0 +- error)
+    MinecraftVector(1.0, 1.0, 0.0).pitch should ===(-45.0f +- error)
+    MinecraftVector(0.0, 1.0, 1.0).pitch should ===(-45.0f +- error)
 
-    MinecraftVector(1.0, -1.0, 0.0).pitch should ===(45.0 +- error)
-    MinecraftVector(0.0, -1.0, 1.0).pitch should ===(45.0 +- error)
+    MinecraftVector(1.0, -1.0, 0.0).pitch should ===(45.0f +- error)
+    MinecraftVector(0.0, -1.0, 1.0).pitch should ===(45.0f +- error)
   }
 }
